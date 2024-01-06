1fe2d1d2ca8b477628b5b6f64fd9748dbfec5031
unknown <jacob@pattersons.net>
2024-01-06 13:11:46 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
         element: <App/>,
     children: [
         {
-            path: "/",
+            path: "/home",
             element: <Home/>
         },
         {
